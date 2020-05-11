a2db0b6c198bbf7f93abf48a176441ece90fd6df
Bruno Cecatto <bruno-ceccatto@hotmail.com>
2020-05-11 19:10:54 -0300

    Add public API test

--- test/paramsx_test.exs
+++ test/paramsx_test.exs
@@ -1,4 +1,26 @@
 defmodule ParamsxTest do
   use ExUnit.Case
-  doctest Paramsx
+
+  describe "filter/2" do
+    test "filter params with success and atomize keys" do
+      params = %{"a" => "value_a", "b" => "value_b"}
+
+      filters = [required: [:a, :b]]
+
+      response =
+        params
+        |> Paramsx.filter(filters)
+        |> Paramsx.atomize_keys()
+
+      assert response == %{a: "value_a", b: "value_b"}
+    end
+  end
+
+  describe "atomize_keys/1" do
+    test "atomize keys with success" do
+      params = %{"a" => "value_a", "b" => "value_b"}
+
+      assert Paramsx.atomize_keys(params) == %{a: "value_a", b: "value_b"}
+    end
+  end
 end
